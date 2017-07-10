a359a9b7d88c7f6ca4b04a96bda35134cf7e46ea
bokov@uthscsa.edu <bokov@uthscsa.edu>
2017-07-10 15:12:21 -0500

    Added example config file. Rename yours to config.R and do not check it in

---
+++ example.config.R
@@ -0,0 +1,4 @@
+#' Replace the following with the full path to the actual directory that 
+#' resulted after you ran `git clone git@github.com:UTHSCSA-CIRD/paper_kidneycancer`
+#' Here is mine...
+workdir <- '/tmp/paper_kidneycancer/';
