67d4180a563fd9f607d5a8040adfd89fe8b3292d
bokov@uthscsa.edu <bokov@uthscsa.edu>
2017-07-10 14:46:05 -0500

    First commit. These aren't quite the files we will be using, they are copied from a previous project that went through some of the same processes.

---
+++ run.R
@@ -0,0 +1,12 @@
+#' Return a factor containing the top-N levels, and the rest binned
+#' into the specified existing or new level. That level is listed last
+#' in the resulting factor.
+#' @param xx A \code{vector} (required).
+#' @param topn \code{numeric} for the top how many levels to keep (optional, default =4)
+#' @param binto \code{character} which new or existing level to dump the other values in
+cl_bintail <- function(xx,topn=4,binto='other'){
+  counts <- sort(table(xx),decreasing = T);
+  if(is.numeric(binto)) binto <- names(counts)[binto];
+  keep <- setdiff(names(counts)[1:min(length(counts),topn)],binto);
+  factor(ifelse(xx %in% keep, xx, binto),levels=c(keep,binto));
+}
