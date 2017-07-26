c6e04e13c48dbe3cdc04301b63132865a0705cd6
bokov@uthscsa.edu <bokov@uthscsa.edu>
2017-07-26 18:26:37 -0500

    Added the submulti function, which substitutes multiple patterns with an equal number of multiple replacements, using a choice of three methods

--- functions.R
+++ functions.R
@@ -16,3 +16,28 @@ cl_bintail <- function(xx,topn=4,binto='other'){
         ),levels=c(keep,binto)));
 }
 
+#' Take a character vector and perform multiple search-replace 
+#' operations on it.
+#' @param xx A \code{vector} of type \code{character} (required)
+#' @param searchrep A \code{matrix} with two columns of type \code{character} (required). The left column is the pattern and the right, the replacement.
+#' @param method One of 'partial','full', or 'exact'. Controls whether to replace only the matching regexp, replace the entire value that contains a matching regexp, or replace the entire value if it's an exact match.
+submulti <- function(xx,searchrep,method=c('partial','full','exact')){
+  # if no method is specified by the user, this makes it take the first value
+  # if a method is only partially written out, this completes it, and if the
+  # method doesn't match any of the valid possibilities this gives an informativ
+  # error message
+  method<-match.arg(method);
+  # rr is a sequence of integers spanning the rows of the searchrep matrix
+  rr <- 1:nrow(searchrep);
+  # oo will hold the result that this function will return
+  oo <- xx;
+  switch(method
+         ,partial = {for(ii in rr)
+           oo <- gsub(searchrep[ii,1],searchrep[ii,2],oo)}
+         ,full =    {for(ii in rr)
+           oo[grepl(searchrep[ii,1],oo)]<-searchrep[ii,2]}
+         ,exact = {for(ii in rr)
+           oo <- gsub(searchrep[ii,1],searchrep[ii,2],oo,fixed = T)}
+         );
+  oo;
+}
