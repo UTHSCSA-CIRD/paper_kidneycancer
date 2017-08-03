cc090a34e1a581625826718f7a976dd91810801a
bokov@uthscsa.edu <bokov@uthscsa.edu>
2017-08-03 11:22:33 -0500

    Enable just-in-time compiling

--- run.R
+++ run.R
@@ -6,14 +6,17 @@
 #' 
 #' ## Load libraries
 #+ warning=FALSE, message=FALSE
-rq_libs <- c('survival','MASS','Hmisc','zoo'       # various analysis methods
+rq_libs <- c('compiler'                            # just-in-time compilation
+             ,'survival','MASS','Hmisc','zoo'      # various analysis methods
              ,'readr','dplyr','stringr','magrittr' # data manipulation & piping
              ,'ggplot2','ggfortify','grid'         # plotting
-             ,'stargazer','broom');                        # table formatting
+             ,'stargazer','broom');                # table formatting
 rq_installed <- sapply(rq_libs,require,character.only=T);
 rq_need <- names(rq_installed[!rq_installed]);
 if(length(rq_need)>0) install.packages(rq_need,repos='https://cran.rstudio.com/',dependencies = T);
 sapply(rq_need,require,character.only=T);
+#' Turn JIT to max: pre-compile all closures, `for`, `while`, and `repeat` loops
+enableJIT(3);
 #' ## Load local config file
 source('./config.R');
 source('./metadata.R');
